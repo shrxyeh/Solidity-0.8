6750068d5e378fc3adac8b1e2209326e98ae11b0
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-03-11 00:44:13 +0530

    Create DefaultValues.sol

---
+++ DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract DefaultValues 
+    {
+        bool public b; // false
+        uint public u; // O
+        int public i; // O
+        address public a; // OXOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+        bytes32 public b32; // oxpooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+    }
