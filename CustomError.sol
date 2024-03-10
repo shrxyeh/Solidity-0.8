dbc7d1c2a55e944f6793cef1c14515ea44296e86
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-03-10 19:52:58 +0530

    Create CustomError.sol

---
+++ CustomError.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+// error Unauthorized(address caller);//Can decalre it outside the contract and later it import into other contracts
+
+contract CustomError {
+    error Unauthorized(address caller);//log the address of owner if error
+    address payable owner = payable(msg.sender);
+
+    function withdraw()public view {
+        if (msg.sender != owner) {
+            revert Unauthorized(msg.sender);//plus saves a lot of gas
+        }
+    }
+}
