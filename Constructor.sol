ffe43e445e98c18199aecd91ebe969797459e110
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-03-11 22:34:40 +0530

    Update Constructor.sol

--- Constructor.sol
+++ Constructor.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Constuctor{
+    address public owner;
+    uint public x;
+
+    //It is called only once when we call the contract
+    constructor(uint _x)
+    {
+        owner=msg.sender;
+        x=_x;
+    }
+}
