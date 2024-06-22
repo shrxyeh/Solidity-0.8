495835835c04b257e5fee3fb79e472a38636544a
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-06-22 12:20:21 +0530

    Update Abstract.sol

--- Abstract.sol
+++ Abstract.sol
@@ -1,22 +1,59 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier:MIT
+
 pragma solidity ^0.8.0;
 
 /*
+
 ABSTRACT CONTRACTS
-Abstract Contract is one which contains at least one function without any implementation.
-Such a contract is used as a base contract.
-Generally an abstract contract contains both implemented as well as abstract functions.
+
+Abstract Contract is one which contains at least one function without any implementation. 
+Such a contract is used as a base contract. 
+Generally an abstract contract contains both implemented as well as abstract functions. 
 Derived contract will implement the abstract function and use the existing functions as and when required.
+
 */
+
+// base contract
+
 abstract contract X {
-    // since the function has no implementation we should mark it as virtual
-    function y() public view virtual returns (string memory);
+    function y() public {}
+}
+
+abstract contract Member {
+    string name;
+    uint256 age = 3;
+
+    // this function makes it abstract since it has no body
+    function setName() public virtual returns (string memory) {}
+
+    // if i define one more function -
+    function returnAge() public view returns (uint256) {
+        return age;
+    }
 }
 
 // derived contract
+contract Teacher is Member {
+    function setName() public pure override returns (string memory) {
+        return "Francesca";
+    }
+}
 
-contract Z is X {
-    function y() public pure override returns (string memory) {
-        return "hello";
+/*
+Exercise:
+1. Create an abstract base contract called Calculator with a read only public function that returns integers 
+2. Create a derived contract called Test which derives the Calculator contract and can calculate 1 + 2 and return the result 
+*/
+
+abstract contract Calculator {
+    function getResult() public pure virtual returns (uint256) {}
+}
+
+contract Test is Calculator {
+    function getResult() public pure override returns (uint256) {
+        uint256 a = 1;
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result;
     }
 }
