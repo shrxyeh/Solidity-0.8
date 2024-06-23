6aed8402bc2f769da69cadfec135cf7bb530e935
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-06-23 23:48:23 +0530

    Update Interface.sol

--- Interface.sol
+++ Interface.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Interface Definition
+interface IStorage {
+    function store(uint256 _value) external;
+    function retrieve() external view returns (uint256);
+}
+
+// Storage Contract Implementing the Interface
+contract Storage is IStorage {
+    uint256 private value;
+
+    function store(uint256 _value) external override {
+        value = _value;
+    }
+
+    function retrieve() external view override returns (uint256) {
+        return value;
+    }
+}
+
+// Contract Using the Interface
+contract StorageUser {
+    IStorage private storageContract;
+
+    constructor(address _storageAddress) {
+        storageContract = IStorage(_storageAddress);
+    }
+
+    function storeValue(uint256 _value) public {
+        storageContract.store(_value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return storageContract.retrieve();
+    }
+}
 
